829c2389fcdf704b73a43bbeb8d2a4fc340152b2
Ericoman <eduardoricomanzanera@gmail.com>
2025-01-25 11:16:28 +0100

    Movemento (obrigado deus)

--- Assets/Scripts/Player/SkateMovement.cs
+++ Assets/Scripts/Player/SkateMovement.cs
@@ -1,33 +1,153 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class SkateMovement : MonoBehaviour
 {
     
-    [SerializeField] private float baseSpeed = 100.0f;
+    [SerializeField] private float baseSpeed = 200.0f;
     
     [SerializeField] private float rotationSpeed = 100.0f;
+    [SerializeField]
+    private Transform frontPoint;
+    [SerializeField]
+    private float checkGroundDistance = 0.2f;
 
+    [SerializeField] private float explosionForce = 20f;
+    [SerializeField] private float linearVelocityThreshold = 0.2f;
+    [SerializeField] private float waitForExplosionSeconds = 0.001f;
     private InputActionMap skateActionMap;
     private InputAction skateJumpAction;
     private InputAction skateMoveAction;
+
+    private Rigidbody rb;
+    private bool isGrounded = false;
+    private bool paused = false;
+    private bool exploding = false;
     
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is creinated
+    private void Awake()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
         skateActionMap = InputSystem.actions.FindActionMap("SkateSoap");
         skateJumpAction = skateActionMap.FindAction("Jump");
         skateMoveAction = skateActionMap.FindAction("Move");
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
-        transform.Translate(Vector3.forward * baseSpeed * Time.deltaTime);
-        
-        Vector2 rotationVector = skateMoveAction.ReadValue<Vector2>();
-        
-        
-        transform.Rotate(Vector3.up, rotationVector.x * rotationSpeed * Time.deltaTime);
+        if (paused) return;
+        CheckGround();
+        if (isGrounded)
+        {
+            AlignToSurface();
+            rb.AddForce(transform.forward * (baseSpeed * Time.deltaTime));
+            Vector2 rotationVector = skateMoveAction.ReadValue<Vector2>();
+            // Calculate the rotation angle
+            float rotationAngle = rotationVector.x * rotationSpeed * Time.deltaTime;
+
+            // Create the new rotation
+            Quaternion deltaRotation = Quaternion.Euler(0f, rotationAngle, 0f);
+
+            // Apply the rotation to the Rigidbody
+            rb.MoveRotation(rb.rotation * deltaRotation);
+            RaycastHit hit;
+            if (rb.linearVelocity.magnitude < linearVelocityThreshold && !exploding && Physics.Raycast(frontPoint.position, transform.forward, out hit, 2f))
+            {
+                StartCoroutine(WaitAndExplode_CO());
+
+            }
+        }
+        else
+        {
+            DetectDownside();
+        }
+    }
+    private void CheckGround()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(frontPoint.position, -transform.up, out hit, checkGroundDistance))
+        {
+                rb.useGravity = false;
+                isGrounded = true;
+        }
+        else
+        {
+            
+            rb.useGravity = true;
+            isGrounded = false;
+            
+        }
+    }
+    IEnumerator WaitAndExplode_CO()
+    {
+        exploding = true;
+        yield return new WaitForSeconds(waitForExplosionSeconds);
+        RaycastHit hit;
+        if (rb.linearVelocity.magnitude < linearVelocityThreshold && Physics.Raycast(frontPoint.position, transform.forward, out hit, 2f))
+        {
+            paused = true;
+            StartCoroutine(WaitAndExplode_CO());
+            rb.AddForce(explosionForce*frontPoint.up, ForceMode.Impulse);
+            Quaternion deltaRotation = Quaternion.Euler(0f, 180f, 0f);
+            rb.MoveRotation(rb.rotation * deltaRotation);
+            paused = false;
+        }
+        exploding = false;
+    }
+    IEnumerator WaitAndExplodeDownside_CO()
+    {
+        exploding = true;
+        yield return new WaitForSeconds(waitForExplosionSeconds);
+        RaycastHit hit;
+        if (rb.linearVelocity.magnitude < linearVelocityThreshold && Physics.Raycast(frontPoint.position, transform.up, out hit, 2f))
+        {
+            paused = true;
+            rb.AddForce(hit.normal * explosionForce, ForceMode.Impulse);
+            Quaternion deltaRotation = Quaternion.Euler(0f, 0f, 180f);
+            rb.MoveRotation(rb.rotation * deltaRotation);
+            paused = false;
+        }
+        exploding = false;
+    }
+    IEnumerator WaitAndPause_CO()
+    {
+        yield return new WaitForSeconds(0.5f);
+        paused = true;
+        rb.useGravity = false;
+        rb.angularVelocity = Vector3.zero;
+        rb.linearVelocity = Vector3.zero;
+    }
+    void AlignToSurface()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(frontPoint.position, -transform.up, out hit, 2f))
+        {
+            rb.AddForce(-hit.normal * (4.0f * Time.deltaTime));
+            //Quaternion surfaceRotation = Quaternion.FromToRotation(transform.up, hit.normal);
+            //transform.rotation = surfaceRotation * transform.rotation;
+        }
+            
+            
+    }
+    void DetectDownside()
+    {
+        RaycastHit hit;
+        if(exploding) return;
+        if (rb.linearVelocity.magnitude < linearVelocityThreshold && Physics.Raycast(frontPoint.position, transform.up, out hit, 2f))
+        {
+            StartCoroutine(WaitAndExplodeDownside_CO());
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Debug.Log(other.gameObject.name);
+        // StartCoroutine(WaitAndPause_CO());
     }
 }
